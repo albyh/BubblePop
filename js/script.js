e39e156bedfa378f9d7b0ab56ee719295e089869
albyh <albyheredia@gmail.com>
2016-06-21 19:22:50 -0700

    Added play timer.

--- js/script.js
+++ js/script.js
@@ -2,44 +2,43 @@
 $('document').ready(function () {
 
 
-    var defaults = {
+    var x = {
         'pf': '#playfield',
         'pfHeight': 600,
         'pfWidth': 600,
-        'lives': 5,
+        'pins': 3,
         'bubCount': 0,
+        'startTime': 0,
+        'endTime': 0,
+        'toWin': 25,
         'updateScore': function(n){ 
             if (n > 0){ 
                 ++this.bubCount;
                 $('#score').text(this.bubCount);
+                if (x.bubCount === x.toWin) {
+                    x.endTime = Date.now();
+                    finished();
+                }
             } else if (n < 0) {
-                --this.lives;
-                $('#lives').text(this.lives);
+                --this.pins;
+                $('#lives').text(this.pins);
             } else {
                 $('#score').text(this.bubCount);
-                $('#lives').text(this.lives);
+                $('#lives').text(this.pins);
             }
          }
     };
 
     function init() {
-        
-
-
-        $(defaults.pf).css({
+        $(x.pf).css({
             'margin': 'auto',
-            'height': defaults.pfHeight,
-            'width': defaults.pfWidth,
+            'height': x.pfHeight,
+            'width': x.pfWidth,
             'background-color': 'black'
         });
-        
         setScoreboard();
-
-
         gameMsg("Press Spacebar to play!");
-
         $(document).on('keydown', function (e) {
-
             if (e.keyCode === 32) {
                 removeMsg();
                 $(document).off('keydown');
@@ -47,15 +46,11 @@ $('document').ready(function () {
                 play();
             }
         });
-
-
-
-
-        
     }
 
     function play() {
         var bub = {};
+        x.startTime = Date.now();
         $(document).on('keydown', function (e) {
             console.groupCollapsed('keypress');
             console.log('Current Bub: ' + bub.text);
@@ -65,20 +60,20 @@ $('document').ready(function () {
 
             if (e.keyCode === bub.code) {
                 console.log('CORRECT keypress');
-                defaults.updateScore(1);
+                x.updateScore(1);
             } else {
                 console.log('WRONG keypress');
-                defaults.updateScore(-1);
+                x.updateScore(-1);
             }
 
             bub.kill();
 
-            if (defaults.lives > 0) {
-                console.log('>0...defaults.lives: ' + defaults.lives);
+            if (x.pins > 0) {
+                console.log('>0...x.pins: ' + x.pins);
                 bub = new Bub() 
                 bub.showBub();
             } else {
-                console.log('else...defaults.lives: ' + defaults.lives);
+                console.log('else...x.pins: ' + x.pins);
                 gameMsg( "Game Over" )
             }
         });
@@ -93,19 +88,45 @@ $('document').ready(function () {
         return rand;
     }
 
-    function Bub() {
+    function gameMsg(msg) {
+        $(document).off('keydown');
+        console.log('Game Over');
+        $(x.pf).append("<h1 id='gameMsg'>" + msg + "</h1>");
+        $('#gameMsg').position({
+            my: 'center',
+            at: 'center',
+            of: x.pf
+        });
+    }
 
+    function removeMsg() {
+        $('#gameMsg').remove();
+    }
+
+    function setScoreboard() {
+        $('#scoreboard').css({ "width": x.pfWidth });
+        x.updateScore(0);
+    }
+
+    function finished() {
+        var msg = "You Finished in ", secs = " seconds!",
+        fast = (x.endTime - x.startTime) / 1000;
+
+        gameMsg(msg+fast+secs);
+    }
+
+    function Bub() {
         this.data = randomLetter();
         this.text = this.data.letter;
         this.code = this.data.code;
         this.class = 'html-class';
-        this.id = defaults.bubCount;
+        this.id = x.bubCount;
         this.htmltag = '<h2 />';
         this.topPos = Math.floor(Math.random() * (
-            (defaults.pfHeight * 0.9 - defaults.pfHeight * 0.1) + defaults.pfWidth * 0.1
+            (x.pfHeight * 0.9 - x.pfHeight * 0.1) + x.pfWidth * 0.1
         )) + 'px';
         this.leftPos = Math.floor(Math.random() * (
-            (defaults.pfWidth * 0.9 - defaults.pfWidth * 0.1) + defaults.pfWidth * 0.1
+            (x.pfWidth * 0.9 - x.pfWidth * 0.1) + x.pfWidth * 0.1
         )) + 'px';
     }
 
@@ -113,7 +134,7 @@ $('document').ready(function () {
         $(this.htmltag, {
             'text': this.text,
             'id': this.id
-        }).appendTo(defaults.pf);
+        }).appendTo(x.pf);
 
         $('#' + this.id).css({
             'position': 'relative',
@@ -123,29 +144,11 @@ $('document').ready(function () {
     }
 
     Bub.prototype.kill = function () {
+
+      
+        //$('#' + this.id).toggle("puff");
         $('#' + this.id).remove();
     }
-
-    function gameMsg(msg) {
-        $(document).off('keydown');
-        console.log('Game Over');
-        $(defaults.pf).append("<h1 id='gameMsg'>"+msg+"</h1>");
-
-        $('#gameMsg').position({
-            my: 'center', 
-            at: 'center', 
-            of: defaults.pf});
-    }
-
-    function removeMsg() {
-        $('#gameMsg').remove();
-    }
-
-    function setScoreboard() {
-        $('#scoreboard').css({ "width": defaults.pfWidth });
-        defaults.updateScore(0);
-    }
-
 
     init();
 
